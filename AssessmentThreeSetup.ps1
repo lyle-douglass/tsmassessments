4b7ae7d250932e8b8f8b87853e3fd68c0f71e868
lyle-douglass <49880381+lyle-douglass@users.noreply.github.com>
2019-04-25 15:54:38 -0400

    Create AssessmentThreeSetup.ps1

---
+++ AssessmentThreeSetup.ps1
@@ -0,0 +1,5 @@
+﻿$confirmation = Read-Host "This will remove multiple files/folders for the purpose of TSM Assessment 2. Are you sure you'd like to proceed?"
+if ($confirmation -eq 'y') {
+  # proceed
+}
+
